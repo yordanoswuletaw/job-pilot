22ec2bef4cb52f1cdc03f76c9cc09f5336cdfab3
yordanoswuletaw <yordanoswuletaw@gmail.com>
2025-05-03 23:01:08 +0300

    define job-pilot workflow

--- src/main.py
+++ src/main.py
@@ -1,166 +1,97 @@
-"""
-Goal: Searches for job listings, evaluates relevance based on a CV, and applies
-
-@dev You need to add OPENAI_API_KEY to your environment variables.
-Also you have to install PyPDF2 to read pdf files: pip install PyPDF2
-"""
-
-import asyncio
-import csv
-import logging
 import os
+import profile
 import sys
-from pathlib import Path
-from typing import Optional
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+import logging
+from tarfile import ENCODING
+from tkinter import N
 from dotenv import load_dotenv
+from typing import Literal
+from langchain_core.runnables.config import P
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
 from langchain_together import ChatTogether
-from pydantic import BaseModel, SecretStr
-from PyPDF2 import PdfReader
-
-from browser_use import ActionResult, Agent, Controller
-from browser_use.browser.browser import Browser, BrowserConfig
-from browser_use.browser.context import BrowserContext
 
 # Validate required environment variables
 load_dotenv()
-required_env_vars = ['TOGETHER_API_KEY']
+required_env_vars = ['TOGETHER_API_KEY', 'TAVILY_API_KEY']
 for var in required_env_vars:
 	if not os.getenv(var):
 		raise ValueError(f'{var} is not set. Please add it to your environment variables.')
 
-logger = logging.getLogger(__name__)
-# full screen mode
-controller = Controller()
+profile = None 
+job_list = None
+class State(TypedDict):
+	action: Literal['SETUP', 'FIND', 'APPLY', 'FIND_AND_APPLY']
+	roles: list[str]
 
-# NOTE: This is the path to your cv file
-CV = Path.cwd() / 'data/fake_resume.pdf'
+def setup(state: State):
+	global profile 
+	profile = 'SETUPed'
+	print(profile, '!!!')
+	return state
 
-if not CV.exists():
-	raise FileNotFoundError(f'You need to set the path to your cv file in the CV variable. CV file not found at {CV}')
+def find(state: State):
+	global job_list
+	job_list = ['job 1', 'job2']
+	print(job_list)
+	return state
 
+def apply(state: State):
+	print('Applied!!!')
+	return state
 
-class Job(BaseModel):
-	title: str
-	link: str
-	company: str
-	fit_score: float
-	location: Optional[str] = None
-	salary: Optional[str] = None
-
-
-@controller.action('Save jobs to file - with a score how well it fits to my profile', param_model=Job)
-def save_jobs(job: Job):
-	with open('jobs.csv', 'a', newline='') as f:
-		writer = csv.writer(f)
-		writer.writerow([job.title, job.company, job.link, job.salary, job.location])
-
-	return 'Saved job to file'
-
-
-@controller.action('Read jobs from file')
-def read_jobs():
-	with open('jobs.csv', 'r') as f:
-		return f.read()
+job_pilot_builder = StateGraph(State)
+# take resume then fill out profile fileds for future use
+job_pilot_builder.add_node('setup', setup)
+# find and save opportunities
+job_pilot_builder.add_node('find', find)
+# apply for opportunities
+job_pilot_builder.add_node('apply', apply)
 
 
-@controller.action('Read my cv for context to fill forms')
-def read_cv():
-	pdf = PdfReader(CV)
-	text = ''
-	for page in pdf.pages:
-		text += page.extract_text() or ''
-	logger.info(f'Read cv with {len(text)} characters')
-	return ActionResult(extracted_content=text, include_in_memory=True)
+def init_action_decision(state: State):
+	if state['action'] == 'SETUP' or not profile:
+		return 'setup'
+	elif state['action'] == 'FIND' or not job_list:
+		return 'find'
+	elif state['action'] == 'APPLY':
+		return 'apply'
+	return END
+
+def setup_action_decision(state: State):
+	if state['action'] == 'FIND' or not job_list:
+		return 'find'
+	elif state['action'] == 'APPLY':
+		return 'apply'
+	return END
+
+def find_action_decision(state: State):
+	if not profile:
+		return 'setup'
+	elif state['action'] == 'APPLY':
+		return 'apply'
+	return END
+
+# start of the graph
+job_pilot_builder.add_conditional_edges(START, init_action_decision)
+
+# intermediatetates
+job_pilot_builder.add_conditional_edges('setup', setup_action_decision)
+job_pilot_builder.add_conditional_edges('find', find_action_decision)
+
+# End of the graph
+job_pilot_builder.add_edge('apply', END) 
+job_pilot_builder.add_edge('find', END)
+job_pilot_builder.add_edge('setup', END)
 
 
-@controller.action(
-	'Upload cv to element - call this function to upload if element is not found, try with different index of the same upload element',
-)
-async def upload_cv(index: int, browser: BrowserContext):
-	path = str(CV.absolute())
-	dom_el = await browser.get_dom_element_by_index(index)
+job_pilot = job_pilot_builder.compile()
 
-	if dom_el is None:
-		return ActionResult(error=f'No element found at index {index}')
-
-	file_upload_dom_el = dom_el.get_file_upload_element()
-
-	if file_upload_dom_el is None:
-		logger.info(f'No file upload element found at index {index}')
-		return ActionResult(error=f'No file upload element found at index {index}')
-
-	file_upload_el = await browser.get_locate_element(file_upload_dom_el)
-
-	if file_upload_el is None:
-		logger.info(f'No file upload element found at index {index}')
-		return ActionResult(error=f'No file upload element found at index {index}')
-
-	try:
-		await file_upload_el.set_input_files(path)
-		msg = f'Successfully uploaded file "{path}" to index {index}'
-		logger.info(msg)
-		return ActionResult(extracted_content=msg)
-	except Exception as e:
-		logger.debug(f'Error in set_input_files: {str(e)}')
-		return ActionResult(error=f'Failed to upload file to index {index}')
-
-
-# browser = Browser(
-# 	config=BrowserConfig(
-# 		browser_binary_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-# 		disable_security=True,
-# 	)
-# )
-
-browser = Browser(
-    config=BrowserConfig(
-        browser_type="chromium",  # Uses Playwright’s Chromium
-        headless=False,
-    )
+# Run the agent
+job_pilot.invoke(
+    {'action': 'APPLY', 'roles': []}
 )
 
+from IPython.display import Image, display
 
-
-async def main():
-	# ground_task = (
-	# 	'You are a professional job finder. '
-	# 	'1. Read my cv with read_cv'
-	# 	'2. Read the saved jobs file '
-	# 	'3. start applying to the first link of Amazon '
-	# 	'You can navigate through pages e.g. by scrolling '
-	# 	'Make sure to be on the english version of the page'
-	# )
-	ground_task = (
-		'You are a professional job finder. '
-		'1. Read my cv with read_cv'
-		'find ml internships in and save them to a file'
-		'search at company:'
-	)
-	tasks = [
-		ground_task + '\n' + 'Google',
-		# ground_task + '\n' + 'Amazon',
-		# ground_task + '\n' + 'Apple',
-		# ground_task + '\n' + 'Microsoft',
-		# ground_task
-		# + '\n'
-		# + 'go to https://nvidia.wd5.myworkdayjobs.com/en-US/NVIDIAExternalCareerSite/job/Taiwan%2C-Remote/Fulfillment-Analyst---New-College-Graduate-2025_JR1988949/apply/autofillWithResume?workerSubType=0c40f6bd1d8f10adf6dae42e46d44a17&workerSubType=ab40a98049581037a3ada55b087049b7 NVIDIA',
-		# ground_task + '\n' + 'Meta',
-	]
-	
-	model = ChatTogether(
-		model="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
-	)
-
-	agents = []
-	for task in tasks:
-		agent = Agent(task=task, llm=model, controller=controller, browser=browser)
-		agents.append(agent)
-
-	await asyncio.gather(*[agent.run() for agent in agents])
-
-
-if __name__ == '__main__':
-	asyncio.run(main())
+display(Image(job_pilot.get_graph().draw_mermaid_png()))
